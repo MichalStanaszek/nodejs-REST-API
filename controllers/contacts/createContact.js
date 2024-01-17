3b2c5a9e2c2030b9a278fb504b30485a84d76d7c
MichalStanaszek <michal.stanaszek@gmail.com>
2024-01-18 00:26:46 +0100

    feat: existing contact verification before create

--- controllers/contacts/createContact.js
+++ controllers/contacts/createContact.js
@@ -1,6 +1,6 @@
 import Contact from "../../models/contactSchema.js";
 export const createContact = async (req, res, next) => {
-  const body = req.body;
+  const { name, email, phone } = req.body;
 
   if (Object.keys(req.body).length === 0) {
     res.status(400).json({ message: "missing fields" });
@@ -8,9 +8,29 @@ export const createContact = async (req, res, next) => {
   }
 
   try {
-    const contact = await Contact.create(body);
+    const existedName = await Contact.findOne({ name });
+    const existedEmail = await Contact.findOne({ email });
+    const existedPhone = await Contact.findOne({ phone });
+
+    if (existedName) {
+      return res.status(409).json({
+        message: "There is an contact with name " + name,
+      });
+    }
+    if (existedEmail) {
+      return res.status(409).json({
+        message: "There is an contact with email " + email,
+      });
+    }
+    if (existedPhone) {
+      return res.status(409).json({
+        message: "There is an contact with phone " + phone,
+      });
+    }
+
+    const contact = await Contact.create(req.body);
     return res.status(201).json(contact);
   } catch (error) {
-    res.status(500).json({ message: error.message });
+    return res.status(400).json({ message: error.message });
   }
 };
