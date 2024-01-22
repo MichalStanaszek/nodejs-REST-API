b080bec64db536afa16278af43f4fb92bf053845
MichalStanaszek <michal.stanaszek@gmail.com>
2024-01-23 00:50:32 +0100

    update users\login.js
    - implement JOI validation
    - implement json web token creation

--- controllers/users/login.js
+++ controllers/users/login.js
@@ -1,25 +1,40 @@
 import User from "#models/userSchema.js";
+import jwt from "jsonwebtoken";
+import { schema } from "#validation/validation.js";
 
 export const login = async (req, res) => {
   const { email, password } = req.body;
-  const user = await User.findOne({ email });
 
-  if (!user) {
-    return res.status(401).json({
-      status: "Unauthorized",
-      code: 401,
-      message: "Email or password is invalid",
-    });
+  //ENTRY DATA JOI VALIDATION
+  const { error } = schema.validate(req.body);
+  if (error) {
+    return res.status(400).json({ message: error.details[0].message });
   }
 
-  const isPasswordValid = await user.validPassword(password);
-  if (!isPasswordValid) {
-    return res.status(401).json({
-      status: "Unauthorized",
-      code: 401,
-      message: "Email or password is invalid",
+  try {
+    const user = await User.findOne({ email });
+
+    if (!user || !(await user.validPassword(password))) {
+      return res.status(401).json({
+        status: "Unauthorized",
+        code: 401,
+        message: "Email or password is wrong",
+      });
+    }
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+    return res.status(200).json({
+      status: "Success",
+      code: 200,
+      data: {
+        token,
+      },
+    });
+  } catch (error) {
+    return res.status(400).json({
+      status: "Bad Request",
+      code: 400,
+      message: error.message,
     });
   }
-
-  console.log("udało się");
 };
