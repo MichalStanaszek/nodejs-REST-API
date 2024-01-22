c027c13cd61360317efdad05c9728644e318175a
MichalStanaszek <michal.stanaszek@gmail.com>
2024-01-23 00:49:27 +0100

    refactor: validation schema

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,9 +2,9 @@ import express from "express";
 const router = express.Router();
 
 import { register, login } from "#controllers/users/index.js";
-import { validation } from "#validation/validation.js";
+// import { validation } from "#validation/validation.js";
 
-router.post("/signup", validation, register);
+router.post("/signup", register);
 router.post("/login", login);
 
 export { router as usersRouter };
--- validation/validation.js
+++ validation/validation.js
@@ -1,16 +1,6 @@
 import Joi from "joi";
 
-const schema = Joi.object({
+export const schema = Joi.object({
   email: Joi.string().email().required(),
   password: Joi.string().min(5).required(),
 });
-
-const validation = (req, res, next) => {
-  const { error } = schema.validate(req.body);
-  if (error) {
-    return res.status(400).json(error.message);
-  }
-  next();
-};
-
-export { validation };
