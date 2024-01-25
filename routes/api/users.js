aae341b6e798264a3b1066e19ac00e8ae52a96fe
MichalStanaszek <michal.stanaszek@gmail.com>
2024-01-25 20:02:58 +0100

    create logout endpoint
    endpoint secured with authMiddleware

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,11 @@
 import express from "express";
+import { authMiddleware } from "#middleware/authMiddleware.js";
 const router = express.Router();
 
-import { register, login } from "#controllers/users/index.js";
+import { register, login, logout } from "#controllers/users/index.js";
 
 router.post("/signup", register);
 router.post("/login", login);
+router.get("/logout", authMiddleware, logout);
 
 export { router as usersRouter };
