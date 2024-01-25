de5b6291a3e5ce3993f6dedbf854bd17767d0609
MichalStanaszek <michal.stanaszek@gmail.com>
2024-01-25 20:24:02 +0100

    create currentUser endpoint
    it's showing user date based on active token

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,10 +2,16 @@ import express from "express";
 import { authMiddleware } from "#middleware/authMiddleware.js";
 const router = express.Router();
 
-import { register, login, logout } from "#controllers/users/index.js";
+import {
+  register,
+  login,
+  logout,
+  currentUser,
+} from "#controllers/users/index.js";
 
 router.post("/signup", register);
 router.post("/login", login);
 router.get("/logout", authMiddleware, logout);
+router.get("/current", authMiddleware, currentUser);
 
 export { router as usersRouter };
