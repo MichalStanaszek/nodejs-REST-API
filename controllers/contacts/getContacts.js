c19aa20c8197436eed2e29da10317e880aa4c49f
MichalStanaszek <michal.stanaszek@gmail.com>
2024-01-25 20:47:29 +0100

    add pagination to getContacts.js

--- controllers/contacts/getContacts.js
+++ controllers/contacts/getContacts.js
@@ -2,9 +2,20 @@ import Contact from "#models/contactSchema.js";
 
 export const getContacts = async (req, res, next) => {
   try {
-    const allContacts = await Contact.find();
+    //Default values for pagination
+    const page = parseInt(req.query.page) || 1;
+    const limit = parseInt(req.query.limit) || 20;
+    const skip = (page - 1) * limit;
 
-    return res.status(200).json({ data: allContacts });
+    const allContacts = await Contact.find().skip(skip).limit(limit);
+    const total = await Contact.countDocuments();
+
+    return res.status(200).json({
+      data: allContacts,
+      currentPage: page,
+      numberOfPages: Math.ceil(total / limit),
+      totalItems: total,
+    });
   } catch (error) {
     res.status(404).json({ message: error.message });
   }
