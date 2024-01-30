76d3b893cc25df32e01bc7357ef32efac24fb6dd
MichalStanaszek <michal.stanaszek@gmail.com>
2024-01-30 20:43:59 +0100

    create controller uploadAvatar .js
    logic to be added

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,13 +2,13 @@ import express from "express";
 import { authMiddleware, uploadMiddleware } from "#middleware/index.js";
 const router = express.Router();
 
-
 import {
   register,
   login,
   logout,
   currentUser,
   updateUserSubscription,
+  uploadAvatar,
 } from "#controllers/users/index.js";
 
 router.post("/signup", register);
@@ -16,6 +16,11 @@ router.post("/login", login);
 router.get("/logout", authMiddleware, logout);
 router.get("/current", authMiddleware, currentUser);
 router.patch("/subscription", authMiddleware, updateUserSubscription);
-router.patch("/avatars", uploadMiddleware.single("picture"));
+router.patch(
+  "/avatars",
+  authMiddleware,
+  uploadMiddleware.single("picture"),
+  uploadAvatar
+);
 
 export { router as usersRouter };
