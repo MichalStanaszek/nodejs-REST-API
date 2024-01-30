9fc5792fa4bbf624a9139cdd63bfbdd17a530ce9
MichalStanaszek <michal.stanaszek@gmail.com>
2024-01-30 19:37:07 +0100

    add new exports to \middleware\index.js

--- middleware/index.js
+++ middleware/index.js
@@ -2,3 +2,5 @@ export * from "./jwtStrategyMiddleware.js";
 export * from "./loggerMiddleware.js";
 export * from "./corsMiddleware.js";
 export * from "./errorsMiddleware.js";
+export * from "./authMiddleware.js";
+export * from "./uploadMiddleware.js";
