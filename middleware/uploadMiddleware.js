a559441394f7df298df77182a057766f764f7210
MichalStanaszek <michal.stanaszek@gmail.com>
2024-01-30 19:37:24 +0100

    Create uploadMiddleware.js

---
+++ middleware/uploadMiddleware.js
@@ -0,0 +1,40 @@
+import multer from "multer";
+import path from "path";
+import { createFolderIfNotExist } from "#helpers/createFolderIfNotExist.js";
+
+const tempDir = path.join(process.cwd(), "temp");
+const storageImage = path.join(process.cwd(), "public/avatars");
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, tempDir);
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  },
+});
+
+const extensionWhiteList = [".jpg", ".jpeg", ".png", ".gif"];
+const mimetypeWhiteList = ["image/jpg", "image/jpeg", "image/png", "image/gif"];
+
+export const uploadMiddleware = multer({
+  storage,
+  fileFilter: async (req, file, cb) => {
+    const extension = path.extname(file.originalname).toLocaleLowerCase();
+    const mimetype = file.mimetype;
+
+    if (
+      !extensionWhiteList.includes(extension) ||
+      !mimetypeWhiteList.includes(mimetype)
+    ) {
+      return cb(null, false);
+    }
+    return cb(null, true);
+  },
+  limits: {
+    fileSize: 1048576, //1024*1024=2MB
+  },
+});
+
+createFolderIfNotExist(tempDir);
+createFolderIfNotExist(storageImage);
