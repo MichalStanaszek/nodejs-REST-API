57248ab53432112b1c522bd8d3f2cc7a6cf16525
MichalStanaszek <michal.stanaszek@gmail.com>
2024-01-31 22:40:01 +0100

    Update app.js
    - move creating new folders to uploadMiddleware.js
    
    - add middleware: app.use(express.static("public"));

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 import express from "express";
-import path from "path";
 import { contactsRouter, usersRouter } from "#routes/api/index.js";
-import { createFolderIfNotExist } from "#helpers/createFolderIfNotExist.js";
 import {
   jwtStrategyMiddleware,
   loggerMiddleware,
@@ -10,19 +8,14 @@ import {
   internalError,
 } from "#middleware/index.js";
 
-const tempDir = path.join(process.cwd(), "temp");
-const storageImage = path.join(process.cwd(), "public/avatars");
-
 const app = express();
-
-createFolderIfNotExist(tempDir);
-createFolderIfNotExist(storageImage);
 
 jwtStrategyMiddleware(app);
 loggerMiddleware(app);
 corsMiddleware(app);
 
 app.use(express.json());
+app.use(express.static("public"));
 
 app.use("/api/contacts", contactsRouter);
 app.use("/api/users", usersRouter);
